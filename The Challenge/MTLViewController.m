f4a8c02208494a85a5eecff6215107d96fd1b0e3
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-19 23:43:26 -0400

    Completed the first week's challenges!

--- The Challenge/MTLViewController.m
+++ The Challenge/MTLViewController.m
@@ -18,12 +18,45 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self doProblem1:5];
+    [self doProblem2:5
+           argument2:15];
+    int result = [self doProblem3:4];
+    NSLog(@"The result of problem 3 is %i", result);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)doProblem1:(int)theInteger
+{
+    NSLog(@"Doing problem 1!");
+    for (int x = theInteger; x >= 1; x--)
+    {
+        NSLog(@"%i", x);
+    }
+}
+
+- (void) doProblem2:(int)theFirstInteger argument2:(int)theSecondInteger
+{
+    NSLog(@"Doing problem 2!");
+    for (int x = theSecondInteger;x>=theFirstInteger; x--)
+    {
+        NSLog(@"%i", x);
+    }
+}
+
+- (int) doProblem3:(int)theInteger
+{
+    int theResult = 1;
+    for (int x = theInteger; x > 0; x --)
+    {
+        theResult *= x;
+    }
+    return theResult;
 }
 
 @end
